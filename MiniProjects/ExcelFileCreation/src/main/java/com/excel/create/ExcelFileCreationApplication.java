a7c896cdfa772be1f9a08d619f14bc0063f366c3
Ashwin <ashwinaradhya@gmail.com>
2025-08-29 12:42:48 +0530

    Commented file creation step in main application class

--- MiniProjects/ExcelFileCreation/src/main/java/com/excel/create/ExcelFileCreationApplication.java
+++ MiniProjects/ExcelFileCreation/src/main/java/com/excel/create/ExcelFileCreationApplication.java
@@ -35,36 +35,36 @@ public class ExcelFileCreationApplication implements CommandLineRunner {
 //			System.out.println("File is not present and hence creating it.....");
 //			excelFileService.createExcelFile();
 //		}
-
-		Path filePath = Paths.get("\\\\\\\\ASHWINS\\\\SharedFolder\\\\Orders.xls");
-
-		if (Files.exists(filePath)) {
-			System.out.println("File is present....");
-		} else {
-			System.out.println("File is not present and hence creating it.....");
-			String networkFilePath = "\\\\ASHWINS\\SharedFolder\\Orders.xls";
-
-			try {
-				// Create a File object representing the file on the network drive
-				File networkFile = new File(networkFilePath);
-
-				// Create the new file if it doesn't already exist
-				if (networkFile.createNewFile()) {
-					System.out.println("File created successfully on network drive: " + networkFile.getAbsolutePath());
-
-					// Optional: Write content to the file
-					try (FileWriter writer = new FileWriter(networkFile)) {
-						writer.write("This is some content for the network file.");
-						System.out.println("Content written to the file.");
-					}
-				} else {
-					System.out.println("File already exists on network drive: " + networkFile.getAbsolutePath());
-				}
-
-			} catch (IOException e) {
-				System.err.println("An error occurred while creating or writing to the file on the network drive:");
-				e.printStackTrace();
-			}
-		}
+//
+//		Path filePath = Paths.get("\\\\\\\\ASHWINS\\\\SharedFolder\\\\Orders.xls");
+//
+//		if (Files.exists(filePath)) {
+//			System.out.println("File is present....");
+//		} else {
+//			System.out.println("File is not present and hence creating it.....");
+//			String networkFilePath = "\\\\ASHWINS\\SharedFolder\\Orders.xls";
+//
+//			try {
+//				// Create a File object representing the file on the network drive
+//				File networkFile = new File(networkFilePath);
+//
+//				// Create the new file if it doesn't already exist
+//				if (networkFile.createNewFile()) {
+//					System.out.println("File created successfully on network drive: " + networkFile.getAbsolutePath());
+//
+//					// Optional: Write content to the file
+//					try (FileWriter writer = new FileWriter(networkFile)) {
+//						writer.write("This is some content for the network file.");
+//						System.out.println("Content written to the file.");
+//					}
+//				} else {
+//					System.out.println("File already exists on network drive: " + networkFile.getAbsolutePath());
+//				}
+//
+//			} catch (IOException e) {
+//				System.err.println("An error occurred while creating or writing to the file on the network drive:");
+//				e.printStackTrace();
+//			}
+//		}
 	}
 }
