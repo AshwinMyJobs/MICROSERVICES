8070e3a40241bcbbec6a8d12981f7cd504360fbf
AshwinMyJobs <myjobsash@gmail.com>
2025-09-02 21:26:48 +0530

    Code for router

--- MiniProjects/FRONTEND/product-catalogue-dealer/src/App.js
+++ MiniProjects/FRONTEND/product-catalogue-dealer/src/App.js
@@ -1,103 +1,103 @@
-// import logo from './logo.svg';
-// import './App.css';
-// import MyComponent from './components/ProductsAdd/MyComponent';
-// import AddProduct from './components/ProductsAdd/AddProduct';
-// import PostDataFormAxios from './components/ProductsAdd/PostDataFormAxios';
+import logo from './logo.svg';
+import './App.css';
+import MyComponent from './components/ProductsAdd/MyComponent';
+import AddProduct from './components/ProductsAdd/AddProduct';
+import PostDataFormAxios from './components/ProductsAdd/PostDataFormAxios';
     
-//     function App() {
-//       return (
-//         <>
-//           <div>
-//             <PostDataFormAxios/>
-//           </div>
-//           <div>
-//             <AddProduct />
-//           </div>
-//           <div>
-//             <MyComponent />
-//           </div>
-//         </>
-//       );
-//     }
+    function App() {
+      return (
+        <>
+          <div>
+            <PostDataFormAxios/>
+          </div>
+          <div>
+            <AddProduct />
+          </div>
+          <div>
+            <MyComponent />
+          </div>
+        </>
+      );
+    }
     
-//     export default App;
+    export default App;
 
 
 
-import React from "react";
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Link,
-    useNavigate,
-    Outlet,
-} from "react-router-dom";
+// import React from "react";
+// import {
+//     BrowserRouter as Router,
+//     Routes,
+//     Route,
+//     Link,
+//     useNavigate,
+//     Outlet,
+// } from "react-router-dom";
 
 
-// Home Page Component
-const Home = () => {
-    const navigate = useNavigate();
+// // Home Page Component
+// const Home = () => {
+//     const navigate = useNavigate();
 
-    return (
-        <div>
-            <h2>Home Page</h2>
-            <button onClick={() =>
-                 navigate("/contact")}>Go to Contact</button>
-        </div>
-    );
-};
+//     return (
+//         <div>
+//             <h2>Home Page</h2>
+//             <button onClick={() =>
+//                  navigate("/contact")}>Go to Contact</button>
+//         </div>
+//     );
+// };
 
-// About Page Component 
-const About = () => (
-    <div>
-        <h2>About Page</h2>
-        <nav>
-            <ul>
-                <li>
-                    <Link to="team">Our Team</Link>
-                </li>
-                <li>
-                    <Link to="company">Our Company</Link> 
-                </li>
-            </ul>
-        </nav>
-        <Outlet />
-    </div>
-);
+// // About Page Component 
+// const About = () => (
+//     <div>
+//         <h2>About Page</h2>
+//         <nav>
+//             <ul>
+//                 <li>
+//                     <Link to="team">Our Team</Link>
+//                 </li>
+//                 <li>
+//                     <Link to="company">Our Company</Link> 
+//                 </li>
+//             </ul>
+//         </nav>
+//         <Outlet />
+//     </div>
+// );
 
-// Components for other pages
-const Contact = () => <h2>Contact Page</h2>;
-const Team = () => <h2>Team Page</h2>;
-const Company = () => <h2>Company Page</h2>;
+// // Components for other pages
+// const Contact = () => <h2>Contact Page</h2>;
+// const Team = () => <h2>Team Page</h2>;
+// const Company = () => <h2>Company Page</h2>;
 
-function App() {
-    return (
-        <Router>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/about">About</Link>
-                    </li>
-                    <li>
-                        <Link to="/contact">Contact</Link>
-                    </li>
-                </ul>
-            </nav>
-            {/*Implementing Routes for respective Path */}
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />}>
-                    <Route path="team" element={<Team />} />
-                    <Route path="company" element={<Company />} />
-                </Route>
-                <Route path="/contact" element={<Contact />} />
-            </Routes>
-        </Router>
-    );
-}
+// function App() {
+//     return (
+//         <Router>
+//             <nav>
+//                 <ul>
+//                     <li>
+//                         <Link to="/">Home</Link>
+//                     </li>
+//                     <li>
+//                         <Link to="/about">About</Link>
+//                     </li>
+//                     <li>
+//                         <Link to="/contact">Contact</Link>
+//                     </li>
+//                 </ul>
+//             </nav>
+//             {/*Implementing Routes for respective Path */}
+//             <Routes>
+//                 <Route path="/" element={<Home />} />
+//                 <Route path="/about" element={<PostDataFormAxios />}>
+//                   <Route path="team" element={<Team />} />
+//                   <Route path="company" element={<Company />} />
+//                 </Route>
+//                 <Route path="/contact" element={<AddProduct />} />
+//             </Routes>
+//         </Router>
+//     );
+// }
 
-export default App;
+// export default App;
